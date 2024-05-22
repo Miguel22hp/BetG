2af9865acd117885728be5bb7463c4f46817265b
Javier Sánchez Tirados <78958304+Javisantir@users.noreply.github.com>
2024-05-22 14:48:40 +0200

    Erase logger on controller

--- betunfair/lib/betunfair_web/controllers/bets_controller.ex
+++ betunfair/lib/betunfair_web/controllers/bets_controller.ex
@@ -1,6 +1,5 @@
 defmodule BetunfairWeb.BetsController do
   use BetunfairWeb, :controller
-  require Logger
   alias Betunfair.Market.GestorMarket
   alias Betunfair.Market.OperationsMarket
   alias Betunfair.Bet.OperationsBet
@@ -17,17 +16,14 @@ defmodule BetunfairWeb.BetsController do
         end
         |> Enum.filter(& &1) # Filtramos los mercados nulos
 
-        Logger.info("Markets loaded: #{inspect(markets)}")
         render(conn, "bets.html", markets: markets, selected_market: nil, back_bets: [], lay_bets: [])
       {:error, reason} ->
-        Logger.error("Failed to load markets: #{reason}")
         render(conn, "bets.html", markets: [], error: reason)
     end
   end
 
   def load_bets(conn, %{"market_id" => market_id}) do
     market_id = String.to_integer(market_id)
-    Logger.info("Loading bets for market_id: #{market_id}")
 
     case OperationsMarket.market_pending_backs(market_id) do
       {:ok, back_bet_tuples} ->
@@ -39,8 +35,6 @@ defmodule BetunfairWeb.BetsController do
         end
         |> Enum.filter(& &1) # Filtramos las apuestas nulas
 
-        Logger.info("Back bets: #{inspect(back_bets)}")
-
         case OperationsMarket.market_pending_lays(market_id) do
           {:ok, lay_bet_tuples} ->
             lay_bets = for {odds, bet_id} <- lay_bet_tuples do
@@ -50,8 +44,6 @@ defmodule BetunfairWeb.BetsController do
               end
             end
             |> Enum.filter(& &1) # Filtramos las apuestas nulas
-
-            Logger.info("Lay bets: #{inspect(lay_bets)}")
 
             case GestorMarket.market_list() do
               {:ok, market_ids} ->
@@ -66,17 +58,14 @@ defmodule BetunfairWeb.BetsController do
 
                 render(conn, "bets.html", markets: markets, selected_market: market_id, back_bets: back_bets, lay_bets: lay_bets)
               {:error, reason} ->
-                Logger.error("Failed to reload markets: #{reason}")
                 render(conn, "bets.html", markets: [], error: reason)
             end
 
           {:error, _reason} ->
-            Logger.error("Unable to load lay bets for market_id: #{market_id}")
             render(conn, "bets.html", error: "Unable to load lay bets.")
         end
 
       {:error, _reason} ->
-        Logger.error("Unable to load back bets for market_id: #{market_id}")
         render(conn, "bets.html", error: "Unable to load back bets.")
     end
   end
